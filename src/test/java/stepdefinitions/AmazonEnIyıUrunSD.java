387be0db3473291a6c832887a283ddbcf92967db
hamza <hyilmaz.techproed@gmail.com>
2021-06-27 21:43:59 +0300

    guncel

--- src/test/java/stepdefinitions/AmazonEnIyıUrunSD.java
+++ src/test/java/stepdefinitions/AmazonEnIyıUrunSD.java
@@ -28,11 +28,35 @@ public class AmazonEnIyıUrunSD {
     @Given("kullanici en iyi urunu bulmak icin hesaplama yapar")
     public void kullanici_en_iyi_urunu_bulmak_icin_hesaplama_yapar() {
 
+        int enYuksek = 0;
+
         for(WebElement urun : page.tumUrunler){
-           WebElement isim =  urun.findElement(By.cssSelector(".a-size-medium.a-color-base.a-text-normal"));
-           WebElement fiyat = urun.findElement(By.cssSelector(".a-price-whole"));
-           WebElement puan = urun.findElement(By.cssSelector("[aria-label~='stars']"));
-           WebElement sayi = urun.findElement(By.xpath("//a[@class='a-link-normal']/span[@class='a-size-base']"));
+           try{
+               WebElement isim =  urun.findElement(By.cssSelector(".a-size-medium.a-color-base.a-text-normal"));
+               WebElement fiyat = urun.findElement(By.cssSelector(".a-price-whole")); // "24"
+               WebElement puan = urun.findElement(By.cssSelector("[aria-label~='stars']"));
+               WebElement sayi = urun.findElement(By.xpath("//a[@class='a-link-normal']/span[@class='a-size-base']"));
+
+               // 4.4 out of 5 stars
+               String puanBul[] = puan.getAttribute("aria-label").split(" out");
+               int temelPuan = (int) Float.parseFloat(puanBul[0]); // 4
+               int degerlendirmeSayisi = Integer.parseInt(sayi.getText().replace(",",""));
+               int temelFiyat = Integer.parseInt(fiyat.getText()); // 24
+
+               int sonuc = ( temelPuan * degerlendirmeSayisi ) - ( temelFiyat * 300 );
+
+
+               if(sonuc > enYuksek){
+                   eniyiUrunIsim = isim.getText();
+                   eniyiUrunPuan = ""+temelPuan;
+                   eniyiUrunSayi = ""+degerlendirmeSayisi;
+                   enIyiUrunFiyat = fiyat.getText();
+               }
+           }catch (Exception e){
+
+           }
+
+
         }
 
     }
@@ -41,7 +65,10 @@ public class AmazonEnIyıUrunSD {
 
     @Then("kullanici en iyi urunu konsola yazar")
     public void kullanici_en_iyi_urunu_konsola_yazar() {
-
+        System.out.println("İsim : "+eniyiUrunIsim);
+        System.out.println("Puan : "+eniyiUrunPuan);
+        System.out.println("Fiyat : "+enIyiUrunFiyat);
+        System.out.println("Sayı : "+eniyiUrunSayi);
     }
 
 
--- target/test-classes/features/amazoneniyiurun.feature
+++ target/test-classes/features/amazoneniyiurun.feature
@@ -1,9 +1,14 @@
 Feature: En Iyı Urun
   @amazoneniyiurun
-  Scenario: kullanici arama sonuclarindaki en iyi urunu bulur
+  Scenario Outline: kullanici arama sonuclarindaki en iyi urunu bulur
     Given kullanici amazon sayfasina gider
-    And kullanici kategori olarak "Automotive" secer
-    And kullanici arama kutusuna "car phone holder" yazar
-    And kullanici sonuc sayfasindaki tum urunleri listeler
+    And kullanici kategori olarak "<kategori>" secer
+    And kullanici arama kutusuna "<urun>" yazar
     And kullanici en iyi urunu bulmak icin hesaplama yapar
     Then kullanici en iyi urunu konsola yazar
+    Examples:
+      | kategori | urun |
+      |Automotive|car phone holder|
+      |Baby      |stroller        |
+      |Books     |Java            |
+
